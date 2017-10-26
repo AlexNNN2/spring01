1f5576a98cfe05dc344498cdf92a51a342f93b35
Fursa Oleksandr <oleksandr.f01@gmail.com>
2017-10-26 23:47:24 +0300

    rest controller added

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @RequestMapping(value = "/data", method = RequestMethod.GET)
+    public String getQuery() {
+        return "Test !!!1";
+    }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public String getEmployee() {
+        return "Employee 111";
+    }
+
+}
